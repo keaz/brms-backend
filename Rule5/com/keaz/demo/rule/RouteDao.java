fbbda91525af6e334baf560bdca1d7b9447872fd
brms service <kasun.ranasingh@icloud.com>
2020-11-28 11:17:13 +0530

    Firste Object

---
+++ Rule5/com/keaz/demo/rule/RouteDao.java
@@ -0,0 +1,24 @@
+package com.keaz.demo.rule;
+
+public class RouteDao {
+   
+   private String source;
+   private int type;
+   
+   public void setSource(String source){
+      this.source = source;
+   }
+   
+   public String getSource(){
+      return this.source;
+   }
+   
+   public void setType(int type){
+      this.type = type;
+   }
+   
+   public int getType(){
+      return this.type;
+   }
+   
+}
